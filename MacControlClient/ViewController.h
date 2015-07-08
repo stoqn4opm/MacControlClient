99114c4743fae991ab2ec36129669b20a8e7d1e0
Stoqn Stoqnov <stoqn@Stoqns-Mac-Pro.local>
2015-07-08 15:40:01 +0300

    Initial Commit

---
+++ MacControlClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MacControlClient
+//
+//  Created by Stoqn Stoqnov on 7/8/15.
+//  Copyright (c) 2015 Stoyan Stoyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
